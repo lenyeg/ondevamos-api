7357d141afc27faff5340e65fa0447e0a81fa563
André Vandal <me@andrevandal.dev>
2023-03-21 20:58:48 -0300

    feat: add strapi-plugin-cloudflare-pages

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,6 +6,17 @@ export default ({ env }) => ({
       jwtSecret: env('JWT_SECRET') || randomBytes(16).toString('base64'),
     },
   },
+  'cloudflare-pages': {
+    enabled: true,
+    config: {
+      instances: [
+        {
+          name: "production website",
+          hook_url: env('CF_PAGES_HOOK_URL'),
+        },
+      ]
+    }
+  },
   email: {
     config: {
       provider: 'nodemailer',
